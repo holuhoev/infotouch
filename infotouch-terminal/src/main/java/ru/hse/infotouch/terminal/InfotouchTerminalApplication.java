9c5c46b70215efd31f6ff8a0cad7751bbffb8b01
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-01-27 19:26:41 +0300

    init 2

---
+++ infotouch-terminal/src/main/java/ru/hse/infotouch/terminal/InfotouchTerminalApplication.java
@@ -0,0 +1,14 @@
+package ru.hse.infotouch.terminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfotouchTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfotouchTerminalApplication.class, args);
+	}
+
+}
+
