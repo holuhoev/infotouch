34f981bac9822ad792f282335d5e24f1c4ebb6a5
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-01-27 22:50:47 +0300

    add core module

---
+++ infotouch-core/src/main/java/ru/hse/infotouch/core/InfotouchCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.infotouch.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfotouchCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfotouchCoreApplication.class, args);
+    }
+
+}
