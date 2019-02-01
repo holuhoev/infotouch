4e432a558673f37694f7834855e46f09f1570870
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-02-01 17:13:19 +0300

    Init feed module

---
+++ infotouch-feed/src/main/java/ru/hse/infotouch/feed/InfotouchFeedApplication.java
@@ -0,0 +1,14 @@
+package ru.hse.infotouch.feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfotouchFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfotouchFeedApplication.class, args);
+	}
+
+}
+
