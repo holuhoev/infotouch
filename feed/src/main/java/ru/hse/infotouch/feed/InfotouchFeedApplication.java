9b8d83793324a07b49025a8bc7e80b8409f27a63
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-02-20 22:56:25 +0300

    Set up properties

--- feed/src/main/java/ru/hse/infotouch/feed/InfotouchFeedApplication.java
+++ feed/src/main/java/ru/hse/infotouch/feed/InfotouchFeedApplication.java
@@ -2,9 +2,12 @@ package ru.hse.infotouch.feed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 
 @SpringBootApplication(scanBasePackages = {"ru.hse.infotouch"})
+@PropertySources(value = {@PropertySource("classpath:application-feed.properties")})
 public class InfotouchFeedApplication {
 
     public static void main(String[] args) {
