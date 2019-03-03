4810746d7a2a3f42ccc5abe1525289fd842a0327
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-03-03 16:18:40 +0300

    1. work with whitespace in api

--- core/src/main/java/ru/hse/infotouch/service/LecturerService.java
+++ core/src/main/java/ru/hse/infotouch/service/LecturerService.java
@@ -1,20 +1,29 @@
 package ru.hse.infotouch.service;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hse.infotouch.domain.Lecturer;
 import ru.hse.infotouch.domain.QLecturer;
 import ru.hse.infotouch.repo.LecturerRepository;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class LecturerService {
 
     private final LecturerRepository repository;
     private final QLecturer qLecturer = QLecturer.lecturer;
+
+    private static String removeRedundantSpace(String searchString) {
+        return Arrays.stream(searchString.split(" "))
+                .filter(StringUtils::isNotEmpty)
+                .collect(Collectors.joining(" "));
+    }
 
     @Autowired
     public LecturerService(LecturerRepository repository) {
@@ -29,8 +38,11 @@ public class LecturerService {
             throw new IllegalArgumentException("searchString must not be empty");
         }
 
-        return Lists.newArrayList(repository.findAll(qLecturer.fio.containsIgnoreCase(searchString.trim())));
+        String validatedSearchString = removeRedundantSpace(searchString);
+
+        return Lists.newArrayList(repository.findAll(qLecturer.fio.containsIgnoreCase(validatedSearchString)));
     }
+
 
     public List<Lecturer> saveAll(Iterable<Lecturer> lecturers) {
         return repository.saveAll(lecturers);
