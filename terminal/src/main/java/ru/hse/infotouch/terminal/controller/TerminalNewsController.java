21674d47c81025ca967910d8905de1d9a7bd2dcb
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-04-22 20:45:18 +0300

    fix terminal news api

--- terminal/src/main/java/ru/hse/infotouch/terminal/controller/TerminalNewsController.java
+++ terminal/src/main/java/ru/hse/infotouch/terminal/controller/TerminalNewsController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static java.util.Objects.isNull;
 
 @RestController
-@RequestMapping("/news")
+@RequestMapping("api/news")
 public class TerminalNewsController {
 
     private final NewsService newsService;
