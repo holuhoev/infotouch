5031762b5431ba0437f9ee18ceaa5ac5d241c363
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-04-23 18:09:40 +0300

    try sshpass

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#sshpass -p "c1Lme1SFRaF8" scp terminal/target/terminal.jar root@62.109.23.105:/opt/infotouch/terminal
-sshpass -V
+sudo sshpass -p "c1Lme1SFRaF8" scp terminal/target/terminal.jar root@62.109.23.105:/opt/infotouch/terminal
