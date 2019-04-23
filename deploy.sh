d00752f0d7c05d54eca545d4e62b142ba076cf3f
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-04-23 19:33:49 +0300

    SCP please

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#scp -i ./deploy_key terminal/target/terminal.jar root@62.109.23.105:/opt/infotouch/terminal
+#scp -i ./deploy_key ./terminal/target/terminal.jar root@62.109.23.105:/opt/infotouch/terminal
 pwd
-ls -la
+ls ./terminal/target
