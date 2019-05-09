9ed8d41dc15d96692394d4e7974024333765b6b7
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2019-05-09 15:16:51 +0300

    small fix api endpoint

--- terminal/src/main/java/ru/hse/infotouch/terminal/controller/LessonController.java
+++ terminal/src/main/java/ru/hse/infotouch/terminal/controller/LessonController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/lesson")
+@RequestMapping("/api/lesson")
 public class LessonController {
 
     private final LessonService lessonService;
